00102082c4bc014fb6f64af380e64cee1f280cae
jinchiim <nellroll@syuin.ac.kr>
2023-11-15 17:08:03 +0900

    feat: PlanMenu 추가구현
    - MenuBoard로부터 검증
    - 검증 후 알맞은 String return

--- src/main/java/christmas/domain/PlanMenu.java
+++ src/main/java/christmas/domain/PlanMenu.java
@@ -1,5 +1,6 @@
 package christmas.domain;
 
+import christmas.domain.dto.DateEventDto;
 import christmas.view.input.dto.InputMenuDto;
 import christmas.view.input.error.InputError;
 import christmas.view.input.error.InputIllegalException;
@@ -10,6 +11,8 @@ import java.util.Set;
 
 public class PlanMenu {
 
+    private static final String WEEKDAY_DISCOUNT = "평일 할인: ";
+    private static final String WEEKEND_DISCOUNT = "주말 할인: ";
     private static final int MAXIMUM_MENU_ORDER = 20;
     private final EnumMap<MenuBoard, Integer> menus;
 
@@ -27,6 +30,22 @@ public class PlanMenu {
 
         return new PlanMenu(orderedMenu);
     }
+
+    public int calculateTotalPrice() {
+        return menus.entrySet().stream()
+                .mapToInt(menu -> MenuBoard.calculateTotalPrice(menu.getKey(), menu.getValue()))
+                .sum();
+    }
+
+    public DateEventDto calculateTotalWeekDayDiscount() {
+
+        return new DateEventDto(WEEKDAY_DISCOUNT, MenuBoard.calculateTotalDessertCount(menus));
+    }
+
+    public DateEventDto calculateTotalWeekendDiscount() {
+        return new DateEventDto(WEEKEND_DISCOUNT, MenuBoard.calculateTotalMainCount(menus));
+    }
+
 
     private static void putMenu(final InputMenuDto menuDto, final EnumMap<MenuBoard, Integer> orderedMenu) {
         menuDto.validateNotDuplicateMenu(orderedMenu);
@@ -54,4 +73,8 @@ public class PlanMenu {
         }
     }
 
+    @Override
+    public String toString() {
+        return MenuBoard.toMenuEventString(menus);
+    }
 }
