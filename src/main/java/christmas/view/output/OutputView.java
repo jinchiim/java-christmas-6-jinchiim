776223321e48f7bf19c6dbff6a1bf964771fb6e4
jinchiim <nellroll@syuin.ac.kr>
2023-11-15 16:33:52 +0900

    test: OutputView 추가 구현
    - 이벤트 상세 메시지 출력 view 구현

--- src/main/java/christmas/view/output/OutputView.java
+++ src/main/java/christmas/view/output/OutputView.java
@@ -2,6 +2,19 @@ package christmas.view.output;
 
 public class OutputView {
 
+    private static final String EVENT_START_SYMBOL = "<";
+    private static final String EVENT_END_SYMBOL = ">";
+    private static final String NEW_LINE = "\n";
+    private static final String MONEY_COUNT = "원";
+
+    public static void printStringEventMessage(String value) {
+        System.out.println(value);
+    }
+
+    public static void printMoneyMessage(String value) {
+        System.out.println(value + MONEY_COUNT);
+    }
+
     public static void printMessage(Output outputMessage) {
         System.out.println(outputMessage.message);
     }
@@ -10,4 +23,7 @@ public class OutputView {
         System.out.println(e.getMessage());
     }
 
+    public static void printEventSymbolMessage(Output outputMessage) {
+        System.out.println(NEW_LINE + EVENT_START_SYMBOL + outputMessage.message + EVENT_END_SYMBOL);
+    }
 }
